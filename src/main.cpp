20337c7509e5171476d9786be88a20da0b9d71a4
lucas.lima <lucasribeirolima974@gmail.com>
2025-10-02 11:07:41 -0300

    Feat: Add parsing for complete command line arguments. Adjust cmake to use base64 as a static library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <base64.h>
+#include <cstring>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -17,71 +18,69 @@
 #define ARGV_INPUT_FILE 2
 #define ARGV_OUTPUT_FILE 3
 
-const char* DECODE_MODE = "decode";
-const char* CODE_MODE = "code";
+const char *DECODE_MODE = "decode";
+const char *CODE_MODE = "code";
 
-int main(int argc, char* argv[])
-{
-    if (argc != ARGV_OPTIONS_COUNT)
-    {
-        std::cout << USAGE_MESSAGE << std::endl;
-        return 1;
+int main(int argc, char *argv[]) {
+  if (argc != ARGV_OPTIONS_COUNT) {
+    std::cout << USAGE_MESSAGE << std::endl;
+    return 1;
+  }
+
+  int decode = -1;
+  char *mode = argv[ARGV_MODE_OPTION];
+  if (mode[0] == '-' && mode[1] == 'c') decode = 0;
+  else if (mode[0] == '-' && mode[1] == 'd') decode = 1;
+
+  if (mode[0] == '-' && mode[1] == '-') {
+    if (strcmp(&mode[2], DECODE_MODE) == 0) {
+      decode = 1;
+    }
+    if (strcmp(&mode[2], CODE_MODE) == 0) {
+      decode = 0;
+    }
+  }
+
+  if (decode == -1) {
+    std::cout << "Invalid mode: " << mode << std::endl;
+    std::cout << USAGE_MESSAGE << std::endl;
+    return 1;
+  }
+
+  try {
+    std::cout
+        << "CodecBase64\n"
+        << "Mode: " << ((mode[1] == '-') ? &mode[2] : &mode[1])
+        << "\n" "Input file: " << argv[ARGV_INPUT_FILE]
+        << "\n" "Saving file at: " << argv[ARGV_OUTPUT_FILE]
+        << std::endl;
+
+    std::ifstream infile{argv[ARGV_INPUT_FILE], std::ios::binary};
+    std::ofstream outfile{argv[ARGV_OUTPUT_FILE], std::ios::binary};
+
+    if (!infile.is_open() || !outfile.is_open()) {
+      throw std::runtime_error("Failed to open input/output file");
     }
 
-    bool decode;
-    char* mode = argv[ARGV_MODE_OPTION];
-    if (mode[0] == '-' && mode[1] == 'c') decode = false;
-    else if (mode[0] == '-' && mode[1] == 'd') decode = true;
-    else
-    {
-        std::cout << "Invalid mode: " << mode << std::endl;
-        std::cout << USAGE_MESSAGE << std::endl;
-        return 1;
+    std::ostringstream oss;
+    oss << infile.rdbuf();
+    std::string input_str = oss.str();
+
+    if (decode) {
+      std::vector<unsigned char> decoded_bytes = crypt::base64::decode(input_str.c_str(), input_str.length());
+      outfile.write(reinterpret_cast<char *>(decoded_bytes.data()), decoded_bytes.size());
+    } else {
+      std::string encoded_str =
+          crypt::base64::encode(input_str.c_str(), input_str.length());
+      outfile.write(encoded_str.data(), encoded_str.size());
     }
 
-    try
-    {
-        std::cout
-            << "CodecBase64\n"
-            << "Mode: " << mode
-            << "\n" "Input file: " << argv[ARGV_INPUT_FILE]
-            << "\n" "Saving file at: " << argv[ARGV_OUTPUT_FILE]
-            << std::endl;
+    std::cout << "File saved" << std::endl;
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  } catch (...) {
+    std::cout << "Unknown exception" << std::endl;
+  }
 
-        std::ifstream infile{argv[ARGV_INPUT_FILE], std::ios::binary};
-        std::ofstream outfile{argv[ARGV_OUTPUT_FILE], std::ios::binary};
-
-        if (!infile.is_open() || !outfile.is_open())
-        {
-            throw std::runtime_error("Failed to open input/output file");
-        }
-
-        std::ostringstream oss;
-        oss << infile.rdbuf();
-        std::string input_str = oss.str();
-
-        if (decode)
-        {
-            std::vector<unsigned char> decoded_bytes = crypt::base64::decode(input_str.c_str(), input_str.length());
-            outfile.write(reinterpret_cast<char*>(decoded_bytes.data()), decoded_bytes.size());
-        }
-        else
-        {
-            std::string encoded_str =
-                crypt::base64::encode(input_str.c_str(), input_str.length());
-            outfile.write(encoded_str.data(), encoded_str.size());
-        }
-
-        std::cout << "File saved" << std::endl;
-    }
-    catch (std::exception& e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    catch (...)
-    {
-        std::cout << "Unknown exception" << std::endl;
-    }
-
-    return 0;
+  return 0;
 }
