832ffed413726d128e85fadb479eb346abb4d0ca
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2025-10-02 20:25:39 -0300

    refactor: Remove the internal functions of external interface.

--- include/base64.h
+++ include/base64.h
@@ -10,10 +10,6 @@
 
 namespace codec::base64
 {
-    void _encode_block(const unsigned char* buffer, std::string& encoded);
-
-    void _decode_block(const unsigned char* buffer, std::vector<unsigned char>& decoded);
-
     std::string encode(const char* msg, const size_t& size);
 
     std::vector<unsigned char> decode(const char* encoded, const size_t& size);
